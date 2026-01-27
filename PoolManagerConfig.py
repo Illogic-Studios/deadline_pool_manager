f267c7e929ddf0cb68e7dc9b0277dcbbf8a9a200
Lucas Jalbaud <jalbaudlucas@gmail.com>
2026-01-27 15:20:20 +0100

    Clean config

--- PoolManagerConfig.py
+++ PoolManagerConfig.py
@@ -1,20 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Configuration pour Deadline Dynamic Pools Manager
-"""
-
 # Pool qui doit toujours rester en première position
 PRIORITY_POOL = "urgent"
 
-# Pool qui doit toujours rester en dernière position (non distribuée)
+# Pool qui doit toujours rester en dernière position (non distribué)
 FALLBACK_POOL = "none"
-
-# Options de logging
-LOG_FILE = "pool_changes.log"
-LOG_DETAILED = True
-
-# Options de debug
-DEBUG = False
 
 # Poids pour le calcul du score hardware
 # Score = CPUs × HARDWARE_WEIGHT_CPU + RAM_GB × HARDWARE_WEIGHT_RAM + VRAM_GB × HARDWARE_WEIGHT_VRAM
