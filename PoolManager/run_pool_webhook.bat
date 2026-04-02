18a78759ffd3a481541c8d052b979657c6c94b4c
Lucas Jalbaud <jalbaudlucas@gmail.com>
2026-04-02 15:00:15 +0200

    fix: the .bat webhook file now works

--- PoolManager/run_pool_webhook.bat
+++ PoolManager/run_pool_webhook.bat
@@ -1,8 +1,6 @@
 @echo off
 
-set SCRIPT_PATH=\\10.16.34.40\DeadlineRepository\custom\scripts\General\PoolManager\PoolManagerWebhook.py
+set SCRIPT_PATH=%~dp0PoolManagerWebhook.py
 set PYTHONPATH=R:\pipeline\networkInstall\python_shares\python311_deadline_discord_pkgs\Lib\site-packages
 
 R:\pipeline\networkInstall\python\Python.3.11.9\python.exe %SCRIPT_PATH%
-
-pause
